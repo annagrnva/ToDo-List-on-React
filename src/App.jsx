87ec1eecb003af904b6e0a01f27fc5d5334fbdd8
annagrnva <m-ippolitova@mail.ru>
2025-03-28 22:22:46 +0400

    inChangeInput create

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,45 @@
 import { useState } from 'react'
-
 import './App.css'
 
-function App() {
-  const [task, setTask] = useState('go to sleep')
+export default function App() {
+  const [task, setTask] = useState(['1', '2', '3'])
+  const [newTask, setNewTask] = useState("")
+
+  function onChangeInput(event) {
+    setNewTask(event.target.value)
+  }
+  console.log(onChangeInput)
+
+  function addTask() {
+    setTask(...task, newTask)
+    setNewTask("")
+  }
 
   return (
     <>
+      <h1>To-Do List</h1>
+
       <div>
-      <p>To-Do List</p>
-      <input type="text" placeholder='Enter the text' />
-      <button onClick={'dsf'}>Add</button>
+        <input type="text"
+          placeholder='Enter the text'
+          value={newTask} 
+          onChange={onChangeInput}/>
+    
+        <button onClick={addTask}>Add</button>
+      </div>
+
+      <div>
+        {task.map((todo, index) => (
+          <p> {task}
+            key={index}
+            todo={todo}
+
+            {/* <button onClick={deleteTask}>Delete</button> */}
+          </p>
+        ))}
+
       </div>
     </>
   )
 }
 
-export default App
